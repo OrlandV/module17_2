bca1fe52c00797ee44a1acf0254b90aac1294b3a
Orland <51851150+OrlandV@users.noreply.github.com>
2024-10-01 21:53:06 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from Module17.app.backend.db import Base
+from app.backend.db import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import CreateTable
